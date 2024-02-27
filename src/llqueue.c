474a4b9310d5d30b9c6652c4f9e5c24cc8617685
MountVesuvius <JudeALakkis@gmail.com>
2024-02-27 13:51:30 +1100

    Added enqueue
    Needed to swap to a double llist setup as I realised dequeuing would
    have been a nightmare without it. Also changed head/tail to start/end to
    be a little clearer

--- src/llqueue.c
+++ src/llqueue.c
@@ -1,28 +1,51 @@
+/* Queues - LList Edition
+ * - [ ] Enqueue: Add an item to the end.
+ * - [ ] Dequeue: Remove the item from the beginning.
+ * - [ ] Front: Get the first item.
+ * - [ ] Rear: Get the last item.
+ * - [ ] isEmpty: Check if the queue is empty.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
 typedef struct node {
     int data;
     struct node* next;
+    struct node* prev;
 } Node;
 
 Node* createNode(int data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
     newNode->next = NULL;
+    newNode->prev = NULL;
     return newNode;
 };
 
 typedef struct queue {
-    Node* head;
-    Node* tail;
+    Node* start;
+    Node* end;
     int size;
 } Queue;
 
 void initQueue(Queue* queue) {
-    queue->head = NULL;
-    queue->tail = NULL;
+    queue->start= NULL;
+    queue->end = NULL;
     queue->size = 0;
+}
+
+void enqueue(Queue* queue, int data) {
+    Node* newNode = createNode(data);
+    if (queue->size == 0) {
+        queue->start = newNode;
+        queue->end = newNode;
+    } else {
+        queue->start->prev = newNode;
+        newNode->next = queue->start;
+        queue->start = newNode;
+    }
+    queue->size++;
 }
 
 int main() {
